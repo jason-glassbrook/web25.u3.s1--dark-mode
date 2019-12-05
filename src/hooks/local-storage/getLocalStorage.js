834a10af6058fcf73ca86c5f3c4baed70a0dbbc7
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-04 19:52:25 -0500

    break out {get,set}LocalStorage

---
+++ src/hooks/local-storage/getLocalStorage.js
@@ -0,0 +1,22 @@
+/// internal modules ///
+import { is } from '../../tools/iffy';
+
+/***************************************
+  HOOK
+***************************************/
+export const getLocalStorage = (key, fallback) => {
+  let value = fallback;
+
+  try {
+    const item = window.localStorage.getItem (key);
+    value = is (item) ? JSON.parse(item) : fallback;
+  }
+  catch (error) {
+    console.error ('getLocalStorage', error);
+  }
+
+  return (value);
+}
+
+/**************************************/
+export default getLocalStorage;
