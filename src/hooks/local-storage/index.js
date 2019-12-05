67378dbd7715819c151da03d7242e91961ce1b41
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-04 19:57:20 -0500

    add index for /hooks/local-storage

---
+++ src/hooks/local-storage/index.js
@@ -0,0 +1,15 @@
+/*******************************************************************************
+  HOOKS/STORAGE/LOCAL
+*******************************************************************************/
+
+/// imports ///
+import getLocalStorage from './getLocalStorage';
+import setLocalStorage from './setLocalStorage';
+import useLocalStorage from './useLocalStorage';
+
+/// exports ///
+export default ({
+  getLocalStorage,
+  setLocalStorage,
+  useLocalStorage,
+});
