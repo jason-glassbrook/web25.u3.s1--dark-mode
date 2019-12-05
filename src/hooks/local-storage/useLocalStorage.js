cf5cad611e8d616a3e9ae0a4be4dab063c94f748
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-04 19:53:25 -0500

    move useLocalStorage to /hooks/local-storage

---
+++ src/hooks/local-storage/useLocalStorage.js
@@ -0,0 +1,26 @@
+/// external modules ///
+import { useState } from "react";
+
+/// internal modules ///
+import getLocalStorage from './getLocalStorage';
+import setLocalStorage from './setLocalStorage';
+// import iffy from '../../tools/iffy';
+
+/***************************************
+  HOOK
+***************************************/
+export const useLocalStorage = (key, initValue) => {
+  const [value, setValue_] = useState (() => (
+    getLocalStorage (key, initValue)
+  ));
+
+  const setValue = (value) => {
+    setValue_ (value);
+    setLocalStorage (key, value);
+  };
+
+  return [value, setValue];
+};
+
+/**************************************/
+export default useLocalStorage;
