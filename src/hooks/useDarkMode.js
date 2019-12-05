bc6ab16dd4564eaf4f9107f0dad4f401897e83ea
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-05 14:00:43 -0500

    fill out useDarkMode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,19 +3,35 @@ import React from 'react';
 
 /// internal modules ///
 import storage from 'hooks/storage';
-// import iffy from 'tools/iffy';
+import iffy from 'tools/iffy';
 
-/*//////////////////////////////////////
-  TOOLS
-//////////////////////////////////////*/
+/***************************************
+  STATE
+***************************************/
+const fallback = {
+  mode : false,
+};
 
 /***************************************
   HOOK
 ***************************************/
-export const useDarkMode = (initMode) => {
+export const useDarkMode = (selector, initMode) => {
   const [mode, setMode] = storage.local.useState ('dark_mode', initMode);
 
-  React.useEffect (() => {}, [mode]);
+  React.useEffect (() => {
+    console.log (`>>> useDarkMode <<<`);
+    if (iffy.boolean (mode)) {
+      const element = document.querySelector (selector);
+      
+      console.log (`mode :=`, mode);
+      element.classList[iffy.yes (mode) ? 'add' : 'remove'] ('dark-mode');
+    }
+    else {
+      console.warn (`TypeError : mode is not boolean :`, mode);
+      console.warn (`[fallback] mode :=`, fallback.mode);
+      setMode (fallback.mode);
+    }
+  }, [mode]);
 
   return [mode, setMode];
 };
