6a6f61eb46b65cbd2ba72298a89026c161199ce2
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-05 11:46:49 -0500

    add index.js for exports

---
+++ src/hooks/storage/index.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+  HOOKS/STORAGE
+*******************************************************************************/
+
+/// imports ///
+import local from './local';
+import session from './session';
+
+/// exports ///
+export * from './local';
+export * from './session';
+
+export default {
+  local,
+  session,
+};
